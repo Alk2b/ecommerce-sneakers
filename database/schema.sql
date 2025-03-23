4108f67bcdcea6cab817edadcebddd4c073fa6bc
Allan Khebab <allan.khebab@edu.igensia.com>
2025-03-23 11:05:28 +0100

    Mise à jour des URLs des images des produits dans schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -53,9 +53,9 @@ VALUES
 -- Insertion de produits (sneakers)
 INSERT INTO Produits (nom, description, prix, stock, image_url)
 VALUES 
-('Nike Air Max', 'Chaussures de sport confortables', 120.00, 10, 'https://example.com/nike-air-max.jpg'),
-('Adidas Superstar', 'Chaussures classiques en cuir', 100.00, 15, 'https://example.com/adidas-superstar.jpg'),
-('Puma RS-X', 'Chaussures lifestyle modernes', 90.00, 20, 'https://example.com/puma-rsx.jpg');
+('Nike Air Max', 'Chaussures de sport confortables', 120.00, 10, 'https://static.nike.com/a/images/t_PDP_936_v1/f_auto,q_auto:eco/a7f07bf7-7896-48c7-b53d-ab5daf86f84e/NIKE+AIR+MAX+EXCEE.png'),
+('Adidas Superstar', 'Chaussures classiques en cuir', 100.00, 15, 'https://thumblr.uniid.it/product/261690/6f3199611e0a.jpg?width=3840&format=webp&q=75'),
+('Puma RS-X', 'Chaussures lifestyle modernes', 90.00, 20, ' https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/391928/01/sv01/fnd/EEA/fmt/png/Sneakers-RS-X-Triple');
 
 -- Insertion de commandes
 INSERT INTO Commandes (client_id, statut)
